924bf68e8a16913025a9cfebef9543cc8ea31cc9
時語 <a31602603@yahoo.com.hk>
2026-08-10 23:29:55 +0800

    fix: fcDIY

--- extensions/fcDIY.lua
+++ extensions/fcDIY.lua
@@ -8325,7 +8325,7 @@ fcj_lvdai = sgs.General(extension_J, "fcj_lvdai", "wu", 4, true)
 
 fcj_qinguoCard = sgs.CreateSkillCard{
 	name = "fcj_qinguoCard",
-	filter = function(self, targets, to_select)
+	filter = function(self, targets, to_select, player)
 		local targets_list = sgs.PlayerList()
 		for _, target in ipairs(targets) do
 			targets_list:append(target)
@@ -8333,7 +8333,7 @@ fcj_qinguoCard = sgs.CreateSkillCard{
 		local slash = sgs.Sanguosha:cloneCard("slash", sgs.Card_NoSuit, 0)
 		slash:setSkillName("fcj_qinguo")
 		slash:deleteLater()
-		return slash:targetFilter(targets_list, to_select, sgs.Self)
+		return slash:targetFilter(targets_list, to_select, player)
 	end,
 	on_use = function(self, room, source, targets)
 		local targets_list = sgs.SPlayerList()
